f9ca6ba9855a36a944261749ab0ca638014302e8
causefx <causefx@me.com>
2017-03-06 20:46:09 -0700

    Small CSS Changes and Tiny Fixes
    Fixed Chrome Icons adding to homescreen
    Fixed DB Updater to refresh to main screen when complete
    Fixed Language Translations that were not set
    Fixed Translation variable when not using browser
    Fixed bug not causing correct drag for ordering tabs
    Added more subtle animations
    Added Images for Theme Preview
    Changed delete tab icon to Trash icon
    Small CSS changes for settings page

--- updatedb.php
+++ updatedb.php
@@ -118,7 +118,7 @@ function checkDatabase($type, $table, $check) {
         
             (function countdown(remaining) {
                 if(remaining === 0)
-                    location.reload(true);
+                    window.top.location = window.top.location.href.split('#')[0];
                 document.getElementById('countdown').innerHTML = "<center>Page will refresh in <strong>" + remaining + "</strong> seconds [You may close this at anytime]</center>";
                 setTimeout(function(){ countdown(remaining - 1); }, 1000);
             })(10);
