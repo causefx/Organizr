bc57c6414973e54d74dbb0e26316088583dcb869
causefx <causefx@me.com>
2017-09-25 15:52:43 -0700

    more fixes

--- functions.php
+++ functions.php
@@ -255,6 +255,7 @@ else :
 endif;
 // ==== Auth Plugins END ====
 // ==== General Class Definitions START ====
+$userLanguage = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) : "en";
 class setLanguage { 
     private $language = null;
 	   private $langCode = null;
