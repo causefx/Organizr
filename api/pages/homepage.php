89e2c2ce057ed961e449b343125815a238e2eded
causefx <causefx@me.com>
2018-01-29 14:38:13 -0700

    clean js not needed

--- api/pages/homepage.php
+++ api/pages/homepage.php
@@ -2,28 +2,6 @@
 if(file_exists('config'.DIRECTORY_SEPARATOR.'config.php')){
 $pageHomepage = '
 <script>
-$("#owl-demo2").owlCarousel({
-    margin:20,
-    nav:true,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        480:{
-            items:2
-        },
-        700:{
-            items:4
-        },
-        1000:{
-            items:3
-        },
-        1100:{
-            items:5
-        }
-    }
-})
 </script>
 <link href="plugins/bower_components/owl.carousel/owl.carousel.min.css" rel="stylesheet" type="text/css" />
 <link href="plugins/bower_components/owl.carousel/owl.theme.default.css" rel="stylesheet" type="text/css" />
