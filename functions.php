96becf723f718129905ff9b4754367420da08ced
causefx <causefx@me.com>
2018-03-09 09:48:33 -0700

    update to 1.75

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 // ===================================
 // Define Version
- define('INSTALLEDVERSION', '1.7');
+ define('INSTALLEDVERSION', '1.75');
 // ===================================
 $debugOrganizr = true;
 if($debugOrganizr == true && file_exists('debug.php')){ require_once('debug.php'); }
