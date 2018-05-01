5eb6b77e28ac6bfe774abca5a678c80e9b19196e
causefx <causefx@me.com>
2018-05-01 11:54:01 -0700

    Fix mobile list view

--- js/custom.js
+++ js/custom.js
@@ -68,19 +68,7 @@ $(document).ready(function () {
     myLazyLoad = new LazyLoad({
         elements_selector: ".lazyload"
     });
-});
-function pageLoad(){
-    "use strict";
-    //Start Organizr
-    $(function () {
-        if($('#preloader:visible').length == 1){
-            $("#preloader").fadeOut();
-        }
-        myLazyLoad.update();
-    });
-
     /* ===== Collapsible Panels JS ===== */
-
     (function ($, window, document) {
         var panelSelector = '[data-perform="panel-collapse"]',
             panelRemover = '[data-perform="panel-dismiss"]';
@@ -128,6 +116,18 @@ function pageLoad(){
             removeElement();
         });
     }(jQuery, window, document));
+});
+function pageLoad(){
+    "use strict";
+    //Start Organizr
+    $(function () {
+        if($('#preloader:visible').length == 1){
+            $("#preloader").fadeOut();
+        }
+        myLazyLoad.update();
+    });
+
+
 
     /* ===== Tooltip Initialization ===== */
 
@@ -183,107 +183,6 @@ function pageLoad(){
 
 
     /* magnific stuff */
-    $('.image-popup-vertical-fit').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        mainClass: 'mfp-img-mobile',
-        image: {
-            verticalFit: true
-        }
-
-    });
-
-    $('.image-popup-fit-width').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        image: {
-            verticalFit: false
-        }
-    });
-
-    $('.image-popup-no-margins').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-        image: {
-            verticalFit: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300 // don't foget to change the duration also in CSS
-        }
-    });
-
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-            }
-        }
-    });
-
-    $('.zoom-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        closeOnContentClick: false,
-        closeBtnInside: false,
-        mainClass: 'mfp-with-zoom mfp-img-mobile',
-        image: {
-            verticalFit: true,
-            titleSrc: function(item) {
-                return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-            }
-        },
-        gallery: {
-            enabled: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300, // don't foget to change the duration also in CSS
-            opener: function(element) {
-                return element.find('img');
-            }
-        }
-
-    });
-
-    $('#image-popups').magnificPopup({
-          delegate: 'a',
-          type: 'image',
-          removalDelay: 500, //delay removal by X to allow out-animation
-          callbacks: {
-            beforeOpen: function() {
-              // just a hack that adds mfp-anim class to markup
-               this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-               this.st.mainClass = this.st.el.attr('data-effect');
-            }
-          },
-          closeOnContentClick: true,
-          midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-        });
-
-    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-
-        fixedContentPos: false
-    });
     $('.popup-with-form').magnificPopup({
         type: 'inline',
         preloader: true,
@@ -320,23 +219,15 @@ function pageLoad(){
       },
       midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
     });
-    $('.simple-ajax-popup-align-top').magnificPopup({
-        type: 'ajax',
-        alignTop: true,
-        overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
-    });
-
-    $('.simple-ajax-popup').magnificPopup({
-        type: 'ajax'
-    });
-
-    //limit height of calendar list view
-    $('.fc-scroller').slimScroll({
-        height: '270px',
-        position: 'right',
-        size: "5px",
-        color: '#dcdcdc'
-    });
+    if(bowser.mobile !== true){
+        //limit height of calendar list view
+        $('.fc-scroller').slimScroll({
+            height: '270px',
+            position: 'right',
+            size: "5px",
+            color: '#dcdcdc'
+        });
+    }
 }
 /* ===== Sidebar ===== */
 
