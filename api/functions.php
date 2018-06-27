9b4002b101bb3a42fae62f63e1d86be269278264
causefx <causefx@me.com>
2018-06-27 09:17:09 -0700

    Fix #902

--- api/functions.php
+++ api/functions.php
@@ -7,14 +7,14 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 foreach (glob(__DIR__ . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . '*.php') as $filename) {
 	require_once $filename;
 }
-//Cookie name
-$GLOBALS['cookieName'] = 'organizr_token_' . hash('sha256', parseDomain($_SERVER['HTTP_HOST']));
 // Add in default and custom settings
 configLazy();
 // Define Logs and files after db location is set
 if (isset($GLOBALS['dbLocation'])) {
 	$GLOBALS['organizrLog'] = $GLOBALS['dbLocation'] . 'organizrLog.json';
 	$GLOBALS['organizrLoginLog'] = $GLOBALS['dbLocation'] . 'organizrLoginLog.json';
+	//Cookie name
+	$GLOBALS['cookieName'] = 'organizr_token_' . hash('sha256', $GLOBALS['organizrAPI']);
 	//Upgrade Check
 	upgradeCheck();
 }
