be5a11f7358d738659e5f966a359c4fc36c68796
causefx <causefx@me.com>
2018-07-17 18:40:13 -0700

    Update ngxc.php

--- ngxc/api/plugins/config/ngxc.php
+++ ngxc/api/plugins/config/ngxc.php
@@ -1,5 +1,9 @@
 <?php
 return array(
 	'ngxc-enabled' => false,
-	'NGXC_SAVE_PATH' => ''
+	'NGXC_SAVE_PATH' => '',
+	'NGXC_SETTINGS_TYPE' => '',
+	'NGXC_SETTINGS_URL' => '',
+	'NGXC_HOMEPAGE_TYPE' => '',
+	'NGXC_HOMEPAGE_URL' => ''
 );
