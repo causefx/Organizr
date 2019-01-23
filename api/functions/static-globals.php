32904a001feb077e5791c350a9f461672b6dc970
causefx <causefx@me.com>
2019-01-23 15:51:08 -0700

    More debug info

--- api/functions/static-globals.php
+++ api/functions/static-globals.php
@@ -1,7 +1,7 @@
 <?php
 // ===================================
 // Organizr Version
-$GLOBALS['installedVersion'] = '2.0.0-beta.800';
+$GLOBALS['installedVersion'] = '2.0.0-beta.950';
 // ===================================
 // Quick php Version check
 $GLOBALS['minimumPHP'] = '7.0.0';
