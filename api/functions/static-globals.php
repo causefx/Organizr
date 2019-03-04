f0d57a8358c60bae6afa7e60919e978572ce79be
causefx <causefx@me.com>
2019-03-04 16:04:54 -0700

    Fix Clicking on iCal Calendar Item Locks Up GUI (#1092)

--- api/functions/static-globals.php
+++ api/functions/static-globals.php
@@ -1,7 +1,7 @@
 <?php
 // ===================================
 // Organizr Version
-$GLOBALS['installedVersion'] = '2.0.0';
+$GLOBALS['installedVersion'] = '2.0.20';
 // ===================================
 // Quick php Version check
 $GLOBALS['minimumPHP'] = '7.1.3';
