90fa078274e1bec8f8dfd4ea60b8a4be5f400176
causefx <causefx@me.com>
2019-05-03 11:03:50 -0700

    Version 2.0.180

--- api/functions/static-globals.php
+++ api/functions/static-globals.php
@@ -1,7 +1,7 @@
 <?php
 // ===================================
 // Organizr Version
-$GLOBALS['installedVersion'] = '2.0.70';
+$GLOBALS['installedVersion'] = '2.0.180';
 // ===================================
 // Quick php Version check
 $GLOBALS['minimumPHP'] = '7.1.3';
