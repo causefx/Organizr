ca88f146b2b230a050d7ad580e220ba9e50e6730
CauseFX <causefx@me.com>
2020-01-13 13:16:31 -0700

    Version 2.0.362

--- api/functions/static-globals.php
+++ api/functions/static-globals.php
@@ -1,7 +1,7 @@
 <?php
 // ===================================
 // Organizr Version
-$GLOBALS['installedVersion'] = '2.0.325';
+$GLOBALS['installedVersion'] = '2.0.362';
 // ===================================
 // Quick php Version check
 $GLOBALS['minimumPHP'] = '7.1.3';
