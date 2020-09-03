29e6b74d2d667fd4227862bd3644db2c4c9b55dd
CauseFX <gNahmean11!>
2020-09-03 12:17:37 -0700

    added plex servers api endpoint

--- api/v2/routes/plex.php
+++ api/v2/routes/plex.php
@@ -51,3 +51,17 @@ $app->post('/plex/register', function ($request, $response, $args) {
 		->withHeader('Content-Type', 'application/json;charset=UTF-8')
 		->withStatus($GLOBALS['responseCode']);
 });
+$app->get('/plex/servers', function ($request, $response, $args) {
+	
+	$Organizr = ($request->getAttribute('Organizr')) ?? new Organizr();
+	if ($Organizr->checkRoute($request)) {
+		if ($Organizr->qualifyRequest(1, true)) {
+			$Organizr->getPlexServers();
+		}
+		
+	}
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
