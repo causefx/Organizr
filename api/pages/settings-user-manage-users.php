83fad712f7f96d6533e3d8a3c7626ffdc11f1fb2
CauseFX <gNahmean11!>
2020-09-14 15:57:09 -0700

    css changes to user mgmt

--- api/pages/settings-user-manage-users.php
+++ api/pages/settings-user-manage-users.php
@@ -13,7 +13,48 @@ function get_page_settings_user_manage_users($Organizr)
 	}
 	return '
 <script>
+	$(document).ready(function($) {
+        
+    }), jsGrid.setDefaults({
+        tableClass: "jsgrid-table table table-striped table-hover"
+    }), jsGrid.setDefaults("text", {
+        _createTextBox: function() {
+            return $("<input>").attr("type", "text").attr("class", "form-control input-sm")
+        }
+    }), jsGrid.setDefaults("number", {
+        _createTextBox: function() {
+            return $("<input>").attr("type", "number").attr("class", "form-control input-sm")
+        }
+    }), jsGrid.setDefaults("textarea", {
+        _createTextBox: function() {
+            return $("<input>").attr("type", "textarea").attr("class", "form-control")
+        }
+    }), jsGrid.setDefaults("control", {
+        _createGridButton: function(cls, tooltip, clickHandler) {
+            var grid = this._grid;
+            return $("<button>").addClass(this.buttonClass).addClass(cls).attr({
+                type: "button",
+                title: tooltip
+            }).on("click", function(e) {
+                clickHandler(grid, e)
+            })
+        }
+    }), jsGrid.setDefaults("select", {
+        _createSelect: function() {
+            var $result = $("<select>").attr("class", "form-control input-sm"),
+                valueField = this.valueField,
+                textField = this.textField,
+                selectedIndex = this.selectedIndex;
+            return $.each(this.items, function(index, item) {
+                var value = valueField ? item[valueField] : index,
+                    text = textField ? item[textField] : item,
+                    $option = $("<option>").attr("value", value).text(text).appendTo($result);
+                $option.prop("selected", selectedIndex === index)
+            }), $result
+        }
+    });
 	$(function() {
+		
 		pageLength = 10;
 		function onPageSelect(newPageLength) {
             pageLength = newPageLength;
