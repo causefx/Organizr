dfedaa61ca46d20400cc40246597437282b48454
CauseFX <gNahmean11!>
2021-01-22 10:46:12 -0700

    version 2.1.165

--- api/classes/organizr.class.php
+++ api/classes/organizr.class.php
@@ -58,7 +58,7 @@ class Organizr
 	
 	// ===================================
 	// Organizr Version
-	public $version = '2.1.120';
+	public $version = '2.1.165';
 	// ===================================
 	// Quick php Version check
 	public $minimumPHP = '7.2';
