2dbc0f23ccf2211d1067848b67ee4f5389ae4d2e
CauseFX <gNahmean11!>
2021-02-11 16:31:46 -0700

    add new log functions

--- api/functions/log-functions.php
+++ api/functions/log-functions.php
@@ -2,9 +2,23 @@
 
 trait LogFunctions
 {
-
+	public function info($msg, $username = null)
+	{
+		$this->writeLog('info', $msg, $username);
+	}
+	
+	public function error($msg, $username = null)
+	{
+		$this->writeLog('error', $msg, $username);
+	}
+	
+	public function warning($msg, $username = null)
+	{
+		$this->writeLog('warning', $msg, $username);
+	}
+	
+	public function debug($msg, $username = null)
+	{
+		$this->writeLog('debug', $msg, $username);
+	}
 }
-
-
-
-
