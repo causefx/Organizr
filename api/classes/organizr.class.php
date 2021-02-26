0b0424fb49f279a3454c59f919507cc8b78dd8a7
CauseFX <gNahmean11!>
2021-02-26 12:41:34 -0700

    version 2.1.235

--- api/classes/organizr.class.php
+++ api/classes/organizr.class.php
@@ -60,7 +60,7 @@ class Organizr
 	
 	// ===================================
 	// Organizr Version
-	public $version = '2.1.228';
+	public $version = '2.1.235';
 	// ===================================
 	// Quick php Version check
 	public $minimumPHP = '7.2';
