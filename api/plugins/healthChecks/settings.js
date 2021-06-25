2581519f7cd965a13368ee68509f8301fa3edd3b
CauseFX <causefx@me.com>
2021-06-25 15:13:34 -0700

    fix selfhosted healtcheck import issue

--- api/plugins/healthChecks/settings.js
+++ api/plugins/healthChecks/settings.js
@@ -44,9 +44,13 @@ $(document).on('click', '.importNewHCService', function() {
 					}
 					$.each(checks, function(i,v) {
 						let alreadySetup = false;
-						let uuid = v.ping_url;
-						uuid = uuid.match(/([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})/g);
-						uuid = uuid[0];
+						if(typeof v.unique_key !== 'undefined'){
+							var uuid = v.unique_key;
+						}else{
+							var uuid = v.ping_url;
+							uuid = uuid.match(/([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})/g);
+							uuid = uuid[0];
+						}
 						if(uuid){
 							if(checksAlreadySetup){
 								$.each(checksAlreadySetup, function(index,val) {
