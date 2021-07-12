1ed096919c72d0770e67507af1f3a4fa088f0455
CauseFX <causefx@me.com>
2021-07-12 14:16:06 -0700

    add get config item to api

--- api/functions/config-functions.php
+++ api/functions/config-functions.php
@@ -2,5 +2,15 @@
 
 trait ConfigFunctions
 {
-
+	public function getConfigItem($item)
+	{
+		if ($this->config[$item]) {
+			$this->setAPIResponse('success', null, 200, $this->config[$item]);
+			return $this->config[$item];
+		} else {
+			$this->setAPIResponse('error', '${$this->config[$item]} is not defined', 404);
+			return false;
+		}
+		
+	}
 }
