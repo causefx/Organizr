686e9eed7fecfc13ce35affc8fd03a3d8d7e5626
CauseFX <causefx@me.com>
2021-07-23 08:52:31 -0700

    upgrade phpmailer/phpmailer to 6.5.0

--- api/vendor/composer/installed.php
+++ api/vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'd116a5555c0c9912ce0b22091539443ff3a9711a',
+    'reference' => '414dc4c37d9f1ea1739e52365c87922321a4f984',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'd116a5555c0c9912ce0b22091539443ff3a9711a',
+      'reference' => '414dc4c37d9f1ea1739e52365c87922321a4f984',
     ),
     'adldap2/adldap2' => 
     array (
@@ -254,12 +254,12 @@
     ),
     'phpmailer/phpmailer' => 
     array (
-      'pretty_version' => 'v6.4.1',
-      'version' => '6.4.1.0',
+      'pretty_version' => 'v6.5.0',
+      'version' => '6.5.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => '9256f12d8fb0cd0500f93b19e18c356906cbed3d',
+      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
     ),
     'pragmarx/google2fa' => 
     array (
