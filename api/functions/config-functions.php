c0e8805a45d0033f498c0c8628f570370d30acd8
CauseFX <causefx@me.com>
2021-08-17 08:59:37 -0700

    prevent organizrHash from being displayed via API

--- api/functions/config-functions.php
+++ api/functions/config-functions.php
@@ -5,7 +5,11 @@ trait ConfigFunctions
 	public function getConfigItem($item)
 	{
 		if ($this->config[$item]) {
-			$this->setAPIResponse('success', null, 200, $this->config[$item]);
+			$configItem = $this->config[$item];
+			if ($item == 'organizrHash') {
+				$configItem = '***Secure***';
+			}
+			$this->setAPIResponse('success', null, 200, $configItem);
 			return $this->config[$item];
 		} else {
 			$this->setAPIResponse('error', $item . ' is not defined or is blank', 404);
