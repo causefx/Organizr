f5ecaab21743f7192d35604fa05c3346f8edd282
CauseFX <causefx@me.com>
2021-08-18 11:06:54 -0700

    only update tautulli custom names config item if it doesn't match

--- api/homepage/plex.php
+++ api/homepage/plex.php
@@ -854,8 +854,11 @@ trait PlexHomepageItem
 	{
 		if ($this->config['tautulliURL'] && $this->config['tautulliApikey'] && $this->config['homepageUseCustomStreamNames']) {
 			$names = $this->getTautulliFriendlyNames(true);
-			$this->updateConfig(array('homepageCustomStreamNames' => json_encode($names)));
-			$this->config['homepageCustomStreamNames'] = json_encode($names);
+			if (json_encode($names) !== $this->config['homepageCustomStreamNames']) {
+				$this->updateConfig(array('homepageCustomStreamNames' => json_encode($names)));
+				$this->config['homepageCustomStreamNames'] = json_encode($names);
+				$this->debug('Updating Tautulli custom names config item', 'SYSTEM');
+			}
 		}
 	}
 	
