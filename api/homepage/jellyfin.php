9fadd96071002cfa829fc0d6920605798c25de1f
CauseFX <causefx@me.com>
2021-08-20 11:59:10 -0700

    fix jellyin open tab #1633

--- api/homepage/jellyfin.php
+++ api/homepage/jellyfin.php
@@ -128,13 +128,6 @@ trait JellyfinHomepageItem
 						'placeholder' => 'Only use if you have Jellyfin in a reverse proxy'
 					),
 					array(
-						'type' => 'input',
-						'name' => 'jellyfinTabURL',
-						'label' => 'Jellyfin Tab WAN URL',
-						'value' => $this->config['jellyfinTabURL'],
-						'placeholder' => 'http(s)://hostname:port'
-					),
-					array(
 						'type' => 'select',
 						'name' => 'cacheImageSize',
 						'label' => 'Image Cache Size',
@@ -561,8 +554,8 @@ trait JellyfinHomepageItem
 		$jellyfinItem['address'] = $this->userDefinedIdReplacementLink($this->config['homepageJellyfinLink'], $jellfinVariablesForLink);
 		$jellyfinItem['nowPlayingOriginalImage'] = 'api/v2/homepage/image?source=jellyfin&type=' . $jellyfinItem['nowPlayingImageType'] . '&img=' . $jellyfinItem['nowPlayingThumb'] . '&height=' . $nowPlayingHeight . '&width=' . $nowPlayingWidth . '&key=' . $jellyfinItem['nowPlayingKey'] . '$' . $this->randString();
 		$jellyfinItem['originalImage'] = 'api/v2/homepage/image?source=jellyfin&type=' . $jellyfinItem['imageType'] . '&img=' . $jellyfinItem['thumb'] . '&height=' . $height . '&width=' . $width . '&key=' . $jellyfinItem['key'] . '$' . $this->randString();
-		$jellyfinItem['openTab'] = $this->config['jellyfinTabURL'] && $this->config['jellyfinTabName'] ? true : false;
-		$jellyfinItem['tabName'] = $this->config['jellyfinTabName'] ? $this->config['jellyfinTabName'] : '';
+		$jellyfinItem['openTab'] = (bool)$this->config['jellyfinTabName'];
+		$jellyfinItem['tabName'] = $this->config['jellyfinTabName'] ?: '';
 		// Stream info
 		$jellyfinItem['userStream'] = array(
 			'platform' => @(string)$itemDetails['Client'],
