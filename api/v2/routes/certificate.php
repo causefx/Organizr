d4b105af6b65d9ef2f7ffceafc712e8b86c1b1d7
CauseFX <causefx@me.com>
2021-08-26 09:10:50 -0700

    add api endpoint for POST /certificate/custom to upload custom cert

---
+++ api/v2/routes/certificate.php
@@ -0,0 +1,11 @@
+<?php
+$app->post('/certificate/custom', function ($request, $response, $args) {
+	$Organizr = ($request->getAttribute('Organizr')) ?? new Organizr();
+	if ($Organizr->qualifyRequest(1, true)) {
+		$Organizr->uploadCert();
+	}
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
