6b0c16ec0ada0cf990f1a0a1275a57f3f5d151c6
CauseFX <causefx@me.com>
2021-09-30 16:00:26 -0700

    add plex demo data for plex streams and metadata

--- api/functions/demo-functions.php
+++ api/functions/demo-functions.php
@@ -11,8 +11,9 @@ trait DemoFunctions
 		}
 		$path = dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'demo_data' . DIRECTORY_SEPARATOR . $file;
 		if (file_exists($path)) {
-			
 			$data = file_get_contents($path);
+			$path = (strpos($file, '/') !== false) ? explode('/', $file)[0] . '/' : '';
+			$data = $this->userDefinedIdReplacementLink($data, ['plugins/images/cache/' => 'api/demo_data/' . $path . 'images/']);
 			$data = json_decode($data, true);
 			$this->setResponse(200, 'Demo data for file: ' . $file, $data['response']['data']);
 			return $data;
