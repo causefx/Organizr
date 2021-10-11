9494d10c7b93281256dc185fafc3975ae8d31511
TehMuffinMoo <mat.cox@tmmn.uk>
2021-10-11 01:12:54 +0100

    Add ability for non-admin users to use invites

--- api/plugins/invites/api.php
+++ api/plugins/invites/api.php
@@ -239,7 +239,7 @@ $app->get('/plugins/invites', function ($request, $response, $args) {
 	 */
 	$Invites = new Invites();
 	if ($Invites->checkRoute($request)) {
-		if ($Invites->qualifyRequest(1, true)) {
+		if ($Invites->qualifyRequest($Invites->config['INVITES-Auth-include'], true)) {
 			$GLOBALS['api']['response']['data'] = $Invites->_invitesPluginGetCodes();
 		}
 	}
@@ -265,7 +265,7 @@ $app->post('/plugins/invites', function ($request, $response, $args) {
 	 */
 	$Invites = new Invites();
 	if ($Invites->checkRoute($request)) {
-		if ($Invites->qualifyRequest(1, true)) {
+		if ($Invites->qualifyRequest($Invites->config['INVITES-Auth-include'], true)) {
 			$Invites->_invitesPluginCreateCode($Invites->apiData($request));
 		}
 	}
@@ -371,7 +371,7 @@ $app->delete('/plugins/invites/{code}', function ($request, $response, $args) {
 	 */
 	$Invites = new Invites();
 	if ($Invites->checkRoute($request)) {
-		if ($Invites->qualifyRequest(1, true)) {
+		if ($Invites->qualifyRequest($Invites->config['INVITES-Auth-include'], true)) {
 			$Invites->_invitesPluginDeleteCode($args['code']);
 		}
 	}
--- api/plugins/invites/config.php
+++ api/plugins/invites/config.php
@@ -1,10 +1,12 @@
 <?php
 return array(
 	'INVITES-enabled' => false,
+	'INVITES-Auth-include' => '1',
 	'INVITES-type-include' => 'plex',
 	'INVITES-plexLibraries' => '',
 	'INVITES-EmbyTemplate' => '',
 	'INVITES-plex-tv-labels' => '',
 	'INVITES-plex-music-labels' => '',
-	'INVITES-plex-movies-labels' => ''
+	'INVITES-plex-movies-labels' => '',
+	'INVITES-db-version' => '1.1'
 );
