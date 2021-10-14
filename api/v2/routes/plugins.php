0846e22aedb831612894a8d773f44e602bad8ea4
CauseFX <causefx@me.com>
2021-10-14 12:11:12 -0700

    added new plugins/marketplace api endpoint

--- api/v2/routes/plugins.php
+++ api/v2/routes/plugins.php
@@ -54,3 +54,16 @@ $app->delete('/plugins/manage/{plugin}', function ($request, $response, $args) {
 		->withHeader('Content-Type', 'application/json;charset=UTF-8')
 		->withStatus($GLOBALS['responseCode']);
 });
+$app->get('/plugins/marketplace', function ($request, $response, $args) {
+	$Organizr = ($request->getAttribute('Organizr')) ?? new Organizr();
+	if ($Organizr->checkRoute($request)) {
+		if ($Organizr->qualifyRequest(1, true)) {
+			$GLOBALS['api']['response']['data'] = $Organizr->getPluginsMarketplace();
+		}
+		
+	}
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
