11ace36c5f777e3f9fe38c3d12488e47618bf01e
CauseFX <causefx@me.com>
2022-04-08 20:55:07 -0700

    version 2.1.1800

--- api/classes/organizr.class.php
+++ api/classes/organizr.class.php
@@ -65,7 +65,7 @@ class Organizr
 
 	// ===================================
 	// Organizr Version
-	public $version = '2.1.1790';
+	public $version = '2.1.1800';
 	// ===================================
 	// Quick php Version check
 	public $minimumPHP = '7.3';
