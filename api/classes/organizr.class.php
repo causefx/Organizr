19dc6c14c06ab86c0c4f88b831d6b92404e4ccc9
mgomon <mgomon@glassnetworks.net>
2025-08-03 21:05:55 -0400

    Temporarily comment out JellyStatHomepageItem trait for debugging

--- api/classes/organizr.class.php
+++ api/classes/organizr.class.php
@@ -67,7 +67,7 @@ class Organizr
 	use uTorrentHomepageItem;
 	use UptimeKumaHomepageItem;
 	use HomepageUserWatchStats;
-	use JellyStatHomepageItem;
+	//use JellyStatHomepageItem;
 
 	// ===================================
 	// Organizr Version
