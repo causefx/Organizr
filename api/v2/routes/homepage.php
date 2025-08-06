a5d556e02bafae56dea88ebe8dfd7e3c9181a299
mgomon <mgomon@glassnetworks.net>
2025-08-06 13:35:36 -0400

    Add JellyStat metadata API route to homepage routing
    - Added POST /homepage/jellystat/metadata endpoint following the same pattern as other media server metadata routes
    - This resolves the 404 error when clicking on JellyStat poster cards for metadata popups
    - The route calls the getJellyStatMetadata() method that was previously added to the JellyStat plugin

--- api/v2/routes/homepage.php
+++ api/v2/routes/homepage.php
@@ -629,3 +629,11 @@ $app->get('/homepage/jellystat', function ($request, $response, $args) {
 		->withHeader('Content-Type', 'application/json;charset=UTF-8')
 		->withStatus($GLOBALS['responseCode']);
 });
+$app->post('/homepage/jellystat/metadata', function ($request, $response, $args) {
+	$Organizr = ($request->getAttribute('Organizr')) ?? new Organizr();
+	$Organizr->getJellyStatMetadata($Organizr->apiData($request));
+	$response->getBody()->write(jsonE($GLOBALS['api']));
+	return $response
+		->withHeader('Content-Type', 'application/json;charset=UTF-8')
+		->withStatus($GLOBALS['responseCode']);
+});
