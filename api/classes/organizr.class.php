14f78b039227fb71739d4f8108ad0a4512c8be72
causefx@me.com <causefx@me.com>
2026-02-23 14:48:42 -0700

    fix: update version numbers in organizr.class.php and version.json to 2.1.4

--- api/classes/organizr.class.php
+++ api/classes/organizr.class.php
@@ -73,7 +73,7 @@ class Organizr
 
 	// ===================================
 	// Organizr Version
-	public $version = '2.1.3210';
+	public $version = '2.1.4';
 	// ===================================
 	// Quick php Version check
 	public $minimumPHP = '7.4';
