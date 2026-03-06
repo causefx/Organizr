e7983f2f638d0e885961fc09021f4380c2986fe2
causefx@me.com <causefx@me.com>
2026-03-06 09:35:27 -0700

    fix: change oidcDefaultGroupId to string type in default.php

--- api/config/default.php
+++ api/config/default.php
@@ -778,7 +778,7 @@ return [
 	'oidcAutoRedirectLogoutUrl' => '',
 	'oidcAutoCreateUsers' => true,
 	'oidcLinkExistingUsers' => true,
-	'oidcDefaultGroupId' => 4,
+	'oidcDefaultGroupId' => '4',
 	'oidcGroupClaimName' => 'groups',
 	'oidcGroupMappings' => '{}',
 	'oidcGroupMappingMode' => 'first',
